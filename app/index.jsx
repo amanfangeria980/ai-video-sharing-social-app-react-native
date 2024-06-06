86d9c708240bc844fb53a2b82e4f28f6c1683073
amanfangeria980 <amanfangeria980@gmail.com>
2024-06-06 14:25:58 +0530

    Fonts and SplashScreen set

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,15 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Aora!</Text>
+      <Link href={"/profile"} style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
