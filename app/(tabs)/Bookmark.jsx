07d91a2bb030b092a18fba30fb4e0625856697d9
amanfangeria980 <amanfangeria980@gmail.com>
2024-06-07 12:18:32 +0530

    Bottom Tabs

--- app/(tabs)/Bookmark.jsx
+++ app/(tabs)/Bookmark.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Bookmark = () => {
+const bookmark = () => {
   return (
     <View>
-      <Text>Bookmark</Text>
+      <Text>bookmark</Text>
     </View>
   );
 };
 
-export default Bookmark;
+export default bookmark;
--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Profile = () => {
+const profile = () => {
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>profile</Text>
     </View>
   );
 };
 
-export default Profile;
+export default profile;
