56150d5093597f1a04d96ce1c7d6fb74b775341a
amanfangeria980 <amanfangeria980@gmail.com>
2024-06-12 16:26:16 +0530

    signup and signin logic done

--- app/(auth)/signIn.jsx
+++ app/(auth)/signIn.jsx
@@ -1,18 +1,35 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "./../../components/FormField";
 import CustomButton from "./../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { signIn as logIn } from "./../../lib/appwrite";
 const signIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
 
-  const submit = () => {};
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (!form.password || !form.email) {
+      Alert.alert("Error", "Please fill in all the fields");
+    }
+    setIsSubmitting(true);
+    try {
+      await logIn(form.email, form.password);
+      // set it to global state...
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
--- app/(auth)/signUp.jsx
+++ app/(auth)/signUp.jsx
@@ -1,19 +1,34 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "./../../components/FormField";
 import CustomButton from "./../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 const signUp = () => {
   const [form, setForm] = useState({
     username: "",
     email: "",
     password: "",
   });
-
-  const submit = () => {};
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = async () => {
+    if (!form.username || !form.password || !form.email) {
+      Alert.alert("Error", "Please fill in all the fields");
+    }
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+      // set it to global state...
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -46,7 +61,7 @@ const signUp = () => {
             otherStyles="mt-7"
           />
           <CustomButton
-            title="Sign In"
+            title="Sign Up"
             handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
